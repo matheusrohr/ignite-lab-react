ad30d87ad323f2257c084cf341176bd729859514
Matheus Keeler Wanes Rohr <matheus@continentalnet.com>
2022-07-12 12:30:20 -0300

    versão final aula 5

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,31 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useGetLessonQuery } from "../graphql/generated";
 import { Lesson } from "./Lesson";
 
-const GET_LESSON_QUERY = gql`
-    query {
-    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    title
-    slug
-  }
-}
-`
-
-interface GetLessonsQueryResponse {
-    lessons:{
-        id: string
-        title: string
-        slug: string
-        availableAt: string
-        lessonType: 'live' | 'class'
-    }[]
-}
-
-
 export function Siderbar(){
-    const {data} = useQuery<GetLessonsQueryResponse>(GET_LESSON_QUERY)
+    const {data} = useGetLessonQuery()
 
     return(
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,49 +1,21 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-    query GetLessonBySlug ($slug: String) {
-        lesson(where: {slug: $slug}) {
-            title
-            videoId
-            description
-            teacher {
-                bio
-                avatarURL
-                name
-            }
-        }
-    }
-`
-
-interface GetLessonBySlugResponse {
-    lesson: {
-        title: string;
-        videoID: string;
-        description: string;
-        teacher: {
-            bio: string;
-            avatarURL: string;
-            name: string;
-        }
-    }
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
     lessonSlug: string
 }
 
 export function Video(props: VideoProps) {
-    const {data} = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    const {data} = useGetLessonBySlugQuery({
         variables: {
             slug: props.lessonSlug,
         }
     })
    
-    if (!data) {
+    if (!data || !data.lesson) {
         return (
             <div className="flex-1">
                 <p>Carregando ...</p>
@@ -56,7 +28,7 @@ export function Video(props: VideoProps) {
             <div className="bg-black justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId= {data.lesson.videoID} />
+                        <Youtube videoId= {data.lesson.videoId} />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -71,18 +43,19 @@ export function Video(props: VideoProps) {
                         <p className="mt-4 text-gray-200 leading-relaxed">
                         {data.lesson.description}
                         </p>
-                        <div className="flex items-center gap-4 mt-6">
-                            <img
-                                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src={data.lesson.teacher.avatarURL}
-                                alt="foto de perfil"
-                            />
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
+                                <img
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                    src={data.lesson.teacher.avatarURL}
+                                    alt="foto de perfil"
+                                />
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                                </div>
                             </div>
-
-                        </div>
+                        )}
                         <div className="flex items-center gap-4 mt-6">
 
                         </div>
